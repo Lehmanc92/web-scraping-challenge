355bfb8b28a7c4cb11f64eff7643f5cea53b5b28
Christian L <Lehmanc92@gmail.com>
2021-06-18 23:52:51 -0400

    screenshots added

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def default():
+    redirect('/scrape')
     conn = 'mongodb://localhost:27017'
     client = pymongo.MongoClient(conn)
     db = client.mars_db
